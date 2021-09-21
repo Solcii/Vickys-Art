3a9e2b76fc02dd3f4896117384c097fce46f4d00
Solcii <sol081105@gmail.com>
2021-09-21 17:44:03 -0300

    Responsive navbar para celular

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const navHamburguer = document.querySelector(".hamburguer")
+const navMenu = document.querySelector(".nav-menu")
+
+navHamburguer.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible");
+});
+
+console.log(123)
