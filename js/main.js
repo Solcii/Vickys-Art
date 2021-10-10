0dea56f76500f20c362e566b7996b9e1e9bdeae3
Solcii <sol081105@gmail.com>
2021-10-10 16:43:19 -0300

    seccion sobre mi y mejoras generales

--- js/main.js
+++ js/main.js
@@ -1,23 +1,16 @@
-const menuBtn = document.querySelector('.menu-btn');
-const NavMenu = document.querySelector('.nav-menu')
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-    NavMenu.classList.toggle('nav-menu_visible')
-    if (!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-    }
+const MENUBTN = document.querySelector('.menu-btn');
+const NAVMENU = document.querySelector('.nav-menu')
+MENUBTN.addEventListener('click', () => {
+    NAVMENU.classList.toggle('nav-menu_visible')
+    MENUBTN.classList.toggle('open')
 });
 
-const portadaBtn = document.getElementById('boton-portada')
-portadaBtn.addEventListener('click', () => {
-    const element = document.querySelector('#categoria-productos')
-    const top = element.getBoundingClientRect().top - 80
+const PORTADABTN = document.getElementById('boton-portada')
+PORTADABTN.addEventListener('click', () => {
+    const ELEMENT = document.querySelector('#categoria-productos')
+    const TOP = ELEMENT.getBoundingClientRect().TOP - 80
     window.scrollTo({
-        top,
+        TOP,
         behavior: 'smooth'
     })
 });
