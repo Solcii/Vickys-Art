7acf23b55d1478a360eabde79d75b7fbc2592cbb
Solcii <sol081105@gmail.com>
2021-10-14 13:12:24 -0300

    anclas e imagenes definitivas

--- js/products.js
+++ js/products.js
@@ -11,7 +11,7 @@ function carouselItemBuilder(number, category, classname, description) {
     const IMAGE_CONTAINER = document.createElement('div')
     IMAGE_CONTAINER.className = `producto ${classname}`
     const IMAGE = document.createElement('img')
-    IMAGE.src = `../images/Productos/${category}/Seleccionadas/${number}.jpg`
+    IMAGE.src = `../images/Productos/${category}/${number}.jpg`
     IMAGE.alt = `${description}`
     IMAGE_CONTAINER.appendChild(IMAGE)
     return IMAGE_CONTAINER
@@ -87,10 +87,10 @@ function indicadorActivo(indicador, fila, i, selectorIndicador) {
 
 /* Funcion para crear Carousel productos por categoria */
 
-function createCarouselByCategory(selectorCarousel, category, classname, descriptionImg, selectorContenedorCarousel, selectorFlechaIzq, selectorFlechaDer, selectorIndicadores, selectorProduct) {
-    const CAROUSEL_TAZAS_CONTENEDOR = document.getElementById(selectorCarousel);
-    const CANT_FOTOS_TAZAS = 15;
-    const ARRAY_TAZAS = createArray(CANT_FOTOS_TAZAS, CAROUSEL_TAZAS_CONTENEDOR, category, classname, descriptionImg);
+function createCarouselByCategory(selectorCarousel, cantFotos, category, classname, descriptionImg, selectorContenedorCarousel, selectorFlechaIzq, selectorFlechaDer, selectorIndicadores, selectorProduct) {
+    const CAROUSEL_CONTENEDOR = document.getElementById(selectorCarousel);
+    const CANT_FOTOS = cantFotos;
+    const ARRAY_TAZAS = createArray(CANT_FOTOS, CAROUSEL_CONTENEDOR, category, classname, descriptionImg);
     const CAROUSEL_ARROW_TAZAS = createCarouselArrow(selectorContenedorCarousel, selectorFlechaIzq, selectorFlechaDer, selectorIndicadores);
     const INDICADORES_TAZAS = calculoIndicadores(selectorIndicadores, selectorContenedorCarousel, selectorProduct);
     window.addEventListener('resize', () => { calculoIndicadores(selectorIndicadores, selectorContenedorCarousel, selectorProduct) });
@@ -101,7 +101,28 @@ function createCarouselByCategory(selectorCarousel, category, classname, descrip
 /* Creacion carouseles por categoria de producto */
 /* Tazas */
 
-const CREATE_CAROUSEL_TAZAS = createCarouselByCategory('carousel-tazas', 'Tazas', 'producto-tazas', 'foto taza', '.contenedor-carousel-tazas', 'flecha-izquierda-tazas', 'flecha-derecha-tazas', '#indicadores-tazas', '.producto-tazas');
+const CREATE_CAROUSEL_TAZAS = createCarouselByCategory('carousel-tazas', 15, 'Tazas', 'producto-tazas', 'foto taza', '.contenedor-carousel-tazas', 'flecha-izquierda-tazas', 'flecha-derecha-tazas', '#indicadores-tazas', '.producto-tazas');
 
 /* Platos, bandejas y bowls */
-const CREATE_CAROUSEL_PLATOS = createCarouselByCategory('carousel-platos', 'Platos bandejas bowls', 'producto-platos', 'foto plato, producto o bowl', '.contenedor-carousel-platos', 'flecha-izquierda-platos', 'flecha-derecha-platos', '#indicadores-platos', '.producto-platos');
+const CREATE_CAROUSEL_PLATOS = createCarouselByCategory('carousel-platos', 15, 'Platos bandejas bowls', 'producto-platos', 'foto plato, producto o bowl', '.contenedor-carousel-platos', 'flecha-izquierda-platos', 'flecha-derecha-platos', '#indicadores-platos', '.producto-platos');
+
+/* Mates */
+const CREATE_CAROUSEL_MATES = createCarouselByCategory('carousel-mates', 6, 'Mates', 'producto-mates', 'foto mate', '.contenedor-carousel-mates', 'flecha-izquierda-mates', 'flecha-derecha-mates', '#indicadores-mates', '.producto-mates');
+
+/* Sets */
+const CREATE_CAROUSEL_SETS = createCarouselByCategory('carousel-sets', 15, 'Sets', 'producto-sets', 'foto set', '.contenedor-carousel-sets', 'flecha-izquierda-sets', 'flecha-derecha-sets', '#indicadores-sets', '.producto-sets');
+
+/* Macetas */
+const CREATE_CAROUSEL_MACETAS = createCarouselByCategory('carousel-macetas', 8, 'Macetas', 'producto-macetas', 'foto maceta', '.contenedor-carousel-macetas', 'flecha-izquierda-macetas', 'flecha-derecha-macetas', '#indicadores-macetas', '.producto-macetas');
+
+/* Cuadros */
+const CREATE_CAROUSEL_CUADROS = createCarouselByCategory('carousel-cuadros', 7, 'Cuadros', 'producto-cuadros', 'foto cuadro', '.contenedor-carousel-cuadros', 'flecha-izquierda-cuadros', 'flecha-derecha-cuadros', '#indicadores-cuadros', '.producto-cuadros');
+
+/* Imanes */
+const CREATE_CAROUSEL_IMANES = createCarouselByCategory('carousel-imanes', 6, 'Imanes', 'producto-imanes', 'foto imanes', '.contenedor-carousel-imanes', 'flecha-izquierda-imanes', 'flecha-derecha-imanes', '#indicadores-imanes', '.producto-imanes');
+
+/* Cartucheras y neceser */
+const CREATE_CAROUSEL_CARTUCHERAS = createCarouselByCategory('carousel-cartucheras', 15, 'Cartuchera neceser', 'producto-cartucheras', 'foto cartuchera o neceser', '.contenedor-carousel-cartucheras', 'flecha-izquierda-cartucheras', 'flecha-derecha-cartucheras', '#indicadores-cartucheras', '.producto-cartucheras');
+
+/* Otros productos */
+const CREATE_CAROUSEL_OTROS = createCarouselByCategory('carousel-otros', 15, 'Otros', 'producto-otros', 'foto otros productos', '.contenedor-carousel-otros', 'flecha-izquierda-otros', 'flecha-derecha-otros', '#indicadores-otros', '.producto-otros');
